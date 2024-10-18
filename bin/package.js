d13d9a0a139ad74ef69e96be463c61455ff378f7
Mathew <mat999@gmail.com>
2024-10-18 16:38:44 +1100

    fix production build

--- bin/package.js
+++ bin/package.js
@@ -1,11 +1,34 @@
 const fs = require('fs')
 
-const ParserRules = require('../src/RuleParser.ebnf.js')
-const rules = JSON.stringify(ParserRules)
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
 
-fs.writeFileSync('src/RuleParser.ebnf.json', rules)
+let replacements = {}
+const ParserRules = require('../src/RuleParser.ebnf.js')
+RegExp.prototype.toJSON = function(){
+    // generate a random string:
+    const randomString = makeid(256)
+    replacements[randomString] = this
+    return randomString
+}
+
+let rules = JSON.stringify(ParserRules)
+for(const key in replacements){
+    rules = rules.replace(`"${key}"`, replacements[key].toString())
+}
+
+fs.writeFileSync('src/RuleParser.production.ebnf.js', "module.exports="+rules)
 
 const ruleParserJs = fs.readFileSync('src/RuleParser.js', 'utf8')
-const ruleParserJsFixed = ruleParserJs.replace("require('./RuleParser.ebnf.js')", 'require(\'./RuleParser.ebnf.json\')')
+const ruleParserJsFixed = ruleParserJs.replace("require('./RuleParser.ebnf.js')", 'require(\'./RuleParser.production.ebnf.js\')')
 
 fs.writeFileSync('src/RuleParser.production.js', ruleParserJsFixed)
