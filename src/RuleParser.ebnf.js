53f70443dd1aaec90a187140397569d44e7cabf7
Mathew <mat999@gmail.com>
2026-02-17 14:10:22 +1100

    preparing for ebnf templater

--- src/RuleParser.ebnf.js
+++ src/RuleParser.ebnf.js
@@ -5,10 +5,10 @@ statement_main       ::= statement EOF
 logical_operator     ||= AND | OR
 statement            ::= expression (logical_operator expression)*
 expression           ::= not_expression | standard_expression | parenthesis_expression
-parenthesis_expression::= BEGIN_PARENTHESIS WS* statement WS* END_PARENTHESIS
+parenthesis_expression ::= BEGIN_PARENTHESIS WS* statement WS* END_PARENTHESIS
 not_expression       ||= NOT (result | parenthesis_expression)
 standard_expression  ||= result ((WS* eq_approx) | (WS* basic_rhs) | ((WS+ IS)? WS+ between) | (WS+ in_expr))?
-basic_rhs            ::= operator WS*  result
+basic_rhs            ::= operator WS* result
 eq_approx            ::= eq_operator WS* "~" WS* result
 
 PLUS                 ::= "+"
@@ -18,44 +18,63 @@ DIVIDE               ::= "/"
 MODULUS              ::= "%"
 DEFAULT_VAL          ::= "??"
 arithmetic_operator  ::= PLUS | MINUS | MULTIPLY | DIVIDE | MODULUS | DEFAULT_VAL
-arithmetic_operand   ::= fcall | number_time | number
-arithmetic_result    ::= arithmetic_operand WS* arithmetic_operator WS* ( arithmetic_result | arithmetic_operand )
+
+number_atom          ::= number
+number_time_atom     ::= number_time
+tod_atom             ::= number_tod
+dow_atom             ::= dow
+
+arithmetic_operand   ::= fcall | number_time_atom | number_atom
+arithmetic_result    ::= arithmetic_operand WS* arithmetic_operator WS* (arithmetic_result | arithmetic_operand)
 
 simple_result        ::= fcall | value
 result               ::= arithmetic_result | simple_result
-value                ::= false | true | array | time_period | number_time | number | number_tod | string
-BEGIN_ARRAY          ::= WS* #x5B WS*  /* [ left square bracket */
-BEGIN_OBJECT         ::= WS* #x7B WS*  /* { left curly bracket */
-END_ARRAY            ::= WS* #x5D WS*  /* ] right square bracket */
-END_OBJECT           ::= WS* #x7D WS*  /* } right curly bracket */
-NAME_SEPARATOR       ::= WS* #x3A WS*  /* : colon */
-VALUE_SEPARATOR      ::= WS* #x2C WS*  /* , comma */
-WS                   ::= [#x20#x09#x0A#x0D]   /* Space | Tab | \n | \r */
+
+value_atom           ::= false | true | array | time_period | number_time_atom | number_atom | tod_atom | string
+value                ::= value_atom
+
+BEGIN_ARRAY          ::= WS* #x5B WS*  /* [ */
+BEGIN_OBJECT         ::= WS* #x7B WS*  /* { */
+END_ARRAY            ::= WS* #x5D WS*  /* ] */
+END_OBJECT           ::= WS* #x7D WS*  /* } */
+NAME_SEPARATOR       ::= WS* #x3A WS*  /* : */
+VALUE_SEPARATOR      ::= WS* #x2C WS*  /* , */
+WS                   ::= [#x20#x09#x0A#x0D]
 
 operator             ::= GTE | LTE | GT | LT | EQ | NEQ
 eq_operator          ::= EQ | NEQ
 
 BEGIN_ARGUMENT       ::= "("
 END_ARGUMENT         ::= ")"
-
 BEGIN_PARENTHESIS    ::= "("
 END_PARENTHESIS      ::= ")"
 
 BEGIN_IN             ||= "IN"
-
 in_expr              ::= BEGIN_IN WS* BEGIN_PARENTHESIS WS* arguments END_PARENTHESIS
 
 argument             ::= statement WS*
 arguments            ::= argument (WS* "," WS* argument)*
-fname                ::= [a-zA-z0-9]+
+fname                ::= [A-Za-z0-9]+
 fcall                ::= fname WS* BEGIN_ARGUMENT WS* arguments? END_ARGUMENT
 
-between_number       ||= (number_time | number) ((WS+ "AND" WS+) | (WS* "-" WS*)) (number_time | number)
-between_number_time  ||= number_time ((WS+ "AND" WS+) | (WS* "-" WS*)) number_time (WS+ dow_range)?
-between_tod          ||= number_tod ((WS+ "AND" WS+)) number_tod (WS+ dow_range)?
+between_dash_or_and  ::= (WS+ "AND" WS+) | (WS* "-" WS*)
+
+between_number_inner ::= number_time_atom | number_atom
+between_number       ||= between_number_inner between_dash_or_and between_number_inner
+
+between_number_time_inner ::= number_time_atom
+between_number_time  ||= between_number_time_inner between_dash_or_and between_number_time_inner (WS+ dow_range)?
+
+between_tod_inner    ::= tod_atom
+between_tod          ||= between_tod_inner (WS+ "AND" WS+) between_tod_inner (WS+ dow_range)?
+
 between              ||= "BETWEEN" WS+ (between_number | between_tod)
+
 dow                  ||= "MONDAY" | "MON" | "TUESDAY" | "TUE" | "WEDNESDAY" | "WED" | "THURSDAY" | "THU" | "THUR" | "FRIDAY" | "FRI" | "SATURDAY" | "SAT" | "SUNDAY" | "SUN"
-dow_range            ||= "ON" WS+ dow (WS+ "TO" WS+ dow)?
+
+dow_range_inner      ::= dow_atom
+dow_range            ||= "ON" WS+ dow_range_inner (WS+ "TO" WS+ dow_range_inner)?
+
 between_time_only    ||= "BETWEEN" WS+ between_number_time
 between_tod_only     ||= "BETWEEN" WS+ between_tod
 
@@ -70,18 +89,21 @@ IS                   ||= "is"
 EQ                   ::= "==" | "="
 NEQ                  ::= "!="
 NOT                  ||= ("!" WS*) | ("not" WS+)
+
 false                ||= "FALSE"
 null                 ||= "null"
 true                 ||= "TRUE"
+
 array                ::= BEGIN_ARRAY (value (VALUE_SEPARATOR value)*)? END_ARRAY
 
 unit                 ||= "seconds" | "minutes" | "hours" | "weeks" | "days" | "second" | "minute" | "week" | "hour" | "day" | "mins" | "min"
-number               ::= "-"? ([0-9]+) ("." [0-9]+)? ("e" ( "-" | "+" )? ("0" | [1-9] [0-9]*))?
+
+number               ::= "-"? ([0-9]+) ("." [0-9]+)? ("e" ("-" | "+")? ("0" | [1-9] [0-9]*))?
 number_time          ::= number WS+ unit
 number_tod           ::= ([0-9]+) ":" ([0-9]+)
 
-time_period_ago      ||= number_time (WS+ number_time)* WS+ AGO
-time_period_ago_between ||= number_time (WS+ number_time)* WS+ AGO WS+ between_tod_only
+time_period_ago      ||= number_time_atom (WS+ number_time_atom)* WS+ AGO
+time_period_ago_between ||= number_time_atom (WS+ number_time_atom)* WS+ AGO WS+ between_tod_only
 time_period_const    ||= "today" | time_period_ago
 time_period          ::= time_period_ago_between | time_period_const | between_tod_only | between_time_only
 
